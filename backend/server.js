75984ef9de8d38e77a5e0286420ecf3a3567ecf1
barry-2710 <73114649+barry-2710@users.noreply.github.com>
2023-09-04 13:46:12 +0530

    Added few changes

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const dataRoutes = require('./routes/route')
+const cors=require("cors");
 
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
 
 // express App
 const app = express()
+
+
+app.use(cors(corsOptions))
 
 //middleware
 app.use(express.json())
